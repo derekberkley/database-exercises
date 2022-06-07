f8c6dce374591d5a2fa72691e2291d74b6982bdb
Derek <99042983+derekberkley@users.noreply.github.com>
2022-06-07 16:01:52 -0500

    end of day, complete with first section of Basic Statements Exercise

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,5 +1,12 @@
+-- TODO: At the top of albums_seeder.sql be sure to USE the codeup_test_db database.
 USE codeup_test_db;
 
+-- TODO: Use INSERT to add records for all the albums from this list on Wikipedia that claim over 30 million sales
+--  (the first two tables). For sales data, use the 'sales certification' column of the tables, not 'claimed sales'.
+--  For artists listed with 'Various Artists', just use the primary artist's name.
+
+-- TODO: First write your queries as separate INSERT statements for each record and test. You should see no output.
+--  TODO: Refactor your script to use a single INSERT statement for all the records and test it again. Again, this should not generate any output.
 INSERT INTO albums (artist, album_name, release_date, genre, sales)
 VALUES ('Michael Jackson', 'Thriller', 1982, 'Pop, Rock, R&B', 47.3),
        ('AC/DC', 'Back in Black', 1980, 'Hard rock', 26.1),
